b5898a45a3cbe73aeb97d1bdcc99d3cedf652404
wjc <www.18219116799@163.com>
2018-10-20 11:31:05 +0800

    方法方法付付付付付

---
+++ Gitdemo/src/main/java/com/itheima/user.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+/**
+ * Created by IntelliJ IDEA.
+ * 作者: Panda
+ * 时间: 2018-10-20 11:27
+ * 包名: com.itheima
+ * 描述:
+ */
+public class user {
+    private  String   name;
+    private  int age;
+}
